2c83d0db5d0aa776249db002a03ec544b94eb44b
jean-irl <andres.ira.041093@gmail.com>
2022-09-26 11:43:14 -0600

    Rutas establecidas Modificacion App.jsx

--- Eventos2/src/App.jsx
+++ Eventos2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import axios from "axios";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
@@ -13,110 +13,38 @@ import {
   InputLabel,
   TextField,
 } from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import logo from "./img/logo.png";
 import { AccountCircle } from "@mui/icons-material";
 import LockIcon from "@mui/icons-material/Lock";
+import Login from "./components/Login";
+import Usuarios from "./components/Usuarios";
+
+
+
+
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginStatus, setLoginStatus] = useState("");
 
-  const login = async () => {
-    try {
-      // axios
-      // .post("http://localhost:3001/", {
-      //   userName: username,
-      //   password: password,
-      // })  
-      // .then((response) => {
-      //   if (response.data.message) {
-      //     setLoginStatus(response.data.alert);
-      //   } else {
-      //     setLoginStatus(response.data[0].username);
-      //   }
-      // });
-
-      const result = await axios.post("http://localhost:3001/login",{
-        username,
-        password
-      })
-      console.log(result)
-
-    } catch (error) {
-      console.log(error);
-    }
-    
-  };
-
-  const alertaNoImplementado = () => {
-    alert("funcion no implementada");
-  };
-
+  
+  
+  //CODIGO HTML RETORNADO//
   return (
-    <Box
-      border={1}
-      sx={{
-        boxShadow: 4,
-        borderRadius: "16px",
-        bgcolor: "#E0DCDF",
-        height: "77%",
-        width: "500px",
-      }}
-    >
-      <img src={logo} alt="logo" className="logo" />
-      <Grid container>
-        <Grid item md={12} m={2}>
-          <FormControl variant="outlined">
-            <TextField
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              htmlFor="User"
-              label=""
-              placeholder="Usuario"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle></AccountCircle>
-                  </InputAdornment>
-                ),
-              }}
-            >
-              Usuario
-            </TextField>
-          </FormControl>
-        </Grid>
-        <Grid item md={12} m={2}>
-          <FormControl>
-            <TextField
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              htmlFor="Pass"
-              label=""
-              placeholder="Contraeña"
-              type="password"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockIcon></LockIcon>
-                  </InputAdornment>
-                ),
-              }}
-            >
-              Usuario
-            </TextField>
-          </FormControl>
-        </Grid>
-        <Grid item md={12} m={2}>
-          <Button variant="contained" color="primary" onClick={login}>
-            iniciar sesion
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/usuarios' element={<Usuarios />}/>
+        
+      </Routes>
+
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+      </BrowserRouter>
+  </div>
+
+  )
+  
 }
 
 export default App;
---
+++ Eventos2/src/components/Usuarios.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Usuarios() {
+  return (
+    <div><h1>estos es usuarios</h1></div>
+  )
+}
+
+export default Usuarios
