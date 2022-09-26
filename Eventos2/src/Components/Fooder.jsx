d3e1d06e7b78d90f58ddbbfc4b9069ad70a748cf
Cani <anibal0506991@gmail.com>
2022-09-26 11:49:27 -0500

    componentes

---
+++ Eventos2/src/Components/Fooder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fooder = () => {
+  return (
+    <footer>fooder</footer>
+  )
+}
+
+export default Fooder
---
+++ Eventos2/src/Pages/Usuarios.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const Usuarios = () => {
+  return (
+    <div>
+      <Header></Header>
+
+      <Fooder></Fooder>
+    </div>
+  )
+}
+
+export default Usuarios
